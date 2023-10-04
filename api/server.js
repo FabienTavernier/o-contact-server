689bae4b9ca3b894bc30beea3a8eb1a10615c5d7
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2023-10-04 09:14:53 +0200

    add server

---
+++ api/server.js
@@ -0,0 +1,26 @@
+// @see https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
+
+const PORT = 3000
+
+const server = jsonServer.create()
+const router = jsonServer.router('db/db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
+
+// Use default router
+server.use(router)
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
+
+// Export the Server API
+module.exports = server
